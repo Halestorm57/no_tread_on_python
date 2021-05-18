a2cae9ac67e0af0da2b5ea9f7570add16dde2c02
Eric Van Meter <ericvanmeter57@gmail.com>
2021-05-17 19:32:21 -0500

    added block data and mining function

--- python-blockchain/block.py
+++ python-blockchain/block.py
@@ -1,3 +1,24 @@
+import time
+
+
+def mine_block(last_block, data):
+    """
+    Mine a block based on the given last_block and data.
+    Mining refers to using computational power.
+    """
+    timestamp = time.time_ns()
+    last_hash = last_block.hash
+    hash = f'{timestamp}-{last_hash}'
+
+    return Block(timestamp, last_hash, hash, data)
+
+
+def genesis():
+    """
+    Generate the genesis block.
+    """
+    return Block(1, 'genesis_last_hash', 'genesis_hash', [])
+
 
 class Block:
     """
@@ -5,18 +26,30 @@ class Block:
     Store transactions in a blockchain that supports a cryptocurrency.
     """
 
-    def __init__(self, data) -> None:
+    def __init__(self, timestamp, last_hash, hash, data) -> None:
+        self.timestamp = timestamp
+        self.last_hash = last_hash
+        self.hash = hash
         self.data = data
 
     def __repr__(self) -> str:
-        return f'Block - data: {self.data}'
+        return (
+            'Block('
+            f'timestamp: {self.timestamp}, '
+            f'last_hash: {self.last_hash}, '
+            f'hash: {self.hash}, '
+            f'Block - data: {self.data})'
+        )
 
 
 def main():
-    block = Block('foo')
+    genesis_block = genesis()
+    block = mine_block(genesis_block, 'foo')
     print(block)
-    print(f'block.py __name__: {__name__}')
 
 
 if __name__ == '__main__':
+    """
+    Executes program iff executed directly from interpreter
+    """
     main()
--- python-blockchain/blockchain.py
+++ python-blockchain/blockchain.py
@@ -28,4 +28,7 @@ def main():
 
 
 if __name__ == '__main__':
+    """
+    Executes program iff executed directly from interpreter
+    """
     main()
